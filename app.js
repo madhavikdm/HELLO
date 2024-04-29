759a706d96c7fcba8bd7e0edab23bd63866c10ce
madhavikdm <100829584+madhavikdm@users.noreply.github.com>
2024-04-29 11:09:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-const port = 7878 
-const HOST = '172.31.21.97'
+const port = 8080 
+const HOST = '127.0.0.1'
 
 
 app.get('/ping', (req, res) => {
